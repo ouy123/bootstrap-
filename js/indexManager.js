78b67430f68acfda1ad605508d15c8b8f17a9d61
ouy123 <1107140506@qq.com>
2019-11-22 22:24:30 +0800

    第一次上传

---
+++ js/indexManager.js
@@ -0,0 +1,16 @@
+$(function () {
+$(".dl").click(function () {
+
+  if($("#input1").val()!='' && $("#input1").val()){
+    // alert(13213);
+    var url = 'homePage.html';
+    window.open(url);
+    window.location.href = url;
+  }
+})
+
+
+// //  var url = 'https://www.baidu.com';
+//   window.open(url); //新窗口打开
+//   window.location.href = url; //当前窗口打开
+})
